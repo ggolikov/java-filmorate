e98e07367848187fe3f58d749a12628fbf539f09
Grigory Golikov <golikov@nextbi.ru>
2025-10-20 10:50:36 +0300

    add-review: Make review likes/dislikes mutually exclusive

--- src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewService.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.ReviewMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.model.ReviewLike;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.review.ReviewStorage;
@@ -16,6 +17,7 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -66,23 +68,47 @@ public class ReviewService {
     }
 
     public void addLike(int id, int userId) {
-        reviewLikeStorage.addLike(id, userId);
-        updateReviewUsefulAfterLikesCountChange(id, true);
+        Optional<ReviewLike> reviewLike = reviewLikeStorage.getLike(id, userId);
+        Optional<ReviewLike> reviewDislike = reviewDislikeStorage.getDislike(id, userId);
+
+        if (reviewLike.isEmpty()) {
+            if (reviewDislike.isPresent()) {
+                removeDislike(id, userId);
+            }
+            reviewLikeStorage.addLike(id, userId);
+            updateReviewUsefulAfterLikesCountChange(id, true);
+        }
     }
 
     public void removeLike(int id, int userId) {
-        reviewLikeStorage.removeLike(id, userId);
-        updateReviewUsefulAfterLikesCountChange(id, false);
+        Optional<ReviewLike> reviewLike = reviewLikeStorage.getLike(id, userId);
+
+        if (reviewLike.isPresent()) {
+            reviewLikeStorage.removeLike(id, userId);
+            updateReviewUsefulAfterLikesCountChange(id, false);
+        }
     }
 
     public void addDislike(int id, int userId) {
-        reviewDislikeStorage.addDislike(id, userId);
-        updateReviewUsefulAfterLikesCountChange(id, false);
+        Optional<ReviewLike> reviewDislike = reviewDislikeStorage.getDislike(id, userId);
+        Optional<ReviewLike> reviewLike = reviewLikeStorage.getLike(id, userId);
+
+        if (reviewDislike.isEmpty()) {
+            if (reviewLike.isPresent()) {
+                removeLike(id, userId);
+            }
+            reviewDislikeStorage.addDislike(id, userId);
+            updateReviewUsefulAfterLikesCountChange(id, false);
+        }
     }
 
     public void removeDislike(int id, int userId) {
-        reviewDislikeStorage.removeDislike(id, userId);
-        updateReviewUsefulAfterLikesCountChange(id, true);
+        Optional<ReviewLike> reviewDislike = reviewDislikeStorage.getDislike(id, userId);
+
+        if (reviewDislike.isPresent()) {
+            reviewDislikeStorage.removeDislike(id, userId);
+            updateReviewUsefulAfterLikesCountChange(id, true);
+        }
     }
 
     private void updateReviewUsefulAfterLikesCountChange(int id, boolean increment) {
--- src/main/java/ru/yandex/practicum/filmorate/storage/reviewLike/ReviewLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/reviewLike/ReviewLikeStorage.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum.filmorate.storage.reviewLike;
 
+import ru.yandex.practicum.filmorate.model.ReviewLike;
+
+import java.util.Optional;
+
 public interface ReviewLikeStorage {
+    Optional<ReviewLike> getLike(int reviewId, int userId);
+
     void addLike(int reviewId, int userId);
 
     void removeLike(int reviewId, int userId);
